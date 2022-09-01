7cd1694e3ac592ebda4353b606e281f02b2230ab
junkor-1011 <tomoyukizxy@gmail.com>
2022-09-02 00:32:40 +0900

    docs: update documentation for test module

--- src/lib/utils/calc-sample.ts
+++ src/lib/utils/calc-sample.ts
@@ -1,13 +1,19 @@
 /**
+ * @packageDocumentation easy samples of calculation
+ */
+
+/**
  * calculate sum of two numbers.
  * @param a - first argument
  * @param b - second argument
+ * @return sum of a and b
  */
 export const sum2 = (a: number, b: number): number => a + b;
 
 /**
  * calculate sum of multiple numbers.
  * @param a - arguments
+ * @return sum of arguments
  */
 export const sum = (...a: number[]): number => {
   let result = 0;
